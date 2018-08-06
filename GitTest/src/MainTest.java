d5e3a929c2e68ba97747fce760632fac47a65c30
darling2 <perfect828@gmail.com>
2018-08-06 12:07:59 +0900

    hy

--- GitTest/src/MainTest.java
+++ GitTest/src/MainTest.java
@@ -4,6 +4,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		
 		System.out.println("hongdarling");
+		System.out.println("123");
 	}
 
 }
---
+++ GitTest/src/com/com/TestCOM.java
@@ -0,0 +1,8 @@
+package com.com;
+
+public class TestCOM {
+	
+	public void sel() {
+		System.out.println("aaaa");
+	}
+}
